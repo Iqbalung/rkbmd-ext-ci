8e1c41de01ac13fdf0960f25373cc5696cf6acef
IQBAL CAHYO GUMILAR <iqbal.gumelar@siloamhospitals.com>
2023-10-27 01:16:00 +0700

    Update MY_Controller.php

--- api/application/core/MY_Controller.php
+++ api/application/core/MY_Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-//error_reporting(0);
+error_reporting(0);
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Authorization, X-API-KEY, Origin,X-Requested-With, Content-Type, Accept, Access-Control-Requested-Method");
