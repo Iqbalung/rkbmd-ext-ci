aa13e52a2e7780988834db44ce41e4b910740a67
IQBAL CAHYO GUMILAR <iqbal.gumelar@siloamhospitals.com>
2023-10-30 06:45:29 +0700

    Update Pemeliharaan.php

--- api/application/controllers/Pemeliharaan.php
+++ api/application/controllers/Pemeliharaan.php
@@ -1199,7 +1199,7 @@ class Pemeliharaan extends MY_Controller {
 	function save_telaah_sub_kegiatan()
 	{
 		$var = base64_decode(ifunsetempty($_POST,'DATA',''));
-		$var = utf8_encode($var);
+		$var = convert_from_latin1_to_utf8_recursively($var);
 		$var = json_decode($var);
 		print_r($var);
 		echo json_last_error_msg();
@@ -1214,5 +1214,22 @@ class Pemeliharaan extends MY_Controller {
 		
 		echo json_encode($out);
 	}
+
+
+	public static function convert_from_latin1_to_utf8_recursively($dat)
+   {
+      if (is_string($dat)) {
+         return utf8_encode($dat);
+      } elseif (is_array($dat)) {
+         $ret = [];
+         foreach ($dat as $i => $d) $ret[ $i ] = self::convert_from_latin1_to_utf8_recursively($d);
+         return $ret;
+      } elseif (is_object($dat)) {
+         foreach ($dat as $i => $d) $dat->$i = self::convert_from_latin1_to_utf8_recursively($d);
+         return $dat;
+      } else {
+         return $dat;
+      }
+   }
 
 }
