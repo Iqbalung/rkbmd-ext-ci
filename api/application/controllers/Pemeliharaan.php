e8a0b3d51fd74447edb623ee3486ba9f3bf4024c
IQBAL CAHYO GUMILAR <iqbal.gumelar@siloamhospitals.com>
2023-10-30 06:26:24 +0700

    Update Pemeliharaan.php

--- api/application/controllers/Pemeliharaan.php
+++ api/application/controllers/Pemeliharaan.php
@@ -1199,7 +1199,8 @@ class Pemeliharaan extends MY_Controller {
 	function save_telaah_sub_kegiatan()
 	{
 		ini_set('max_input_vars', 10000);
-		print_r(ifunsetempty($_POST,'DATA',''));
+		print_r(base64_decode(ifunsetempty($_POST,'DATA','')));
+		
 		$params = array(
 			'DATA_BARANG_TELAAH' => json_decode(base64_decode(ifunsetempty($_POST,'DATA','')), true),				
 		);	
